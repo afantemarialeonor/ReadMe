a85c70bf20da1eface39c69a026d72d4c07f3c7b
afantemarialeonor <afantemarialeonor@gmail.com>
2026-05-11 04:27:48 +0000

    Day 5

---
+++ Toy.java
@@ -0,0 +1,15 @@
+public class Toy{
+
+public String name;
+String brand ;
+double price;
+int quantity;
+
+public static void main(String[] args) {
+    System.out.println("hello java trainees");
+}
+
+
+
+
+}

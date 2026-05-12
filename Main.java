0f28a55f41f369b6031d7ff687f2281f62cd1f12
afantemarialeonor <afantemarialeonor@gmail.com>
2026-05-12 04:11:12 +0000

    Day 6

--- Main.java
+++ Main.java
@@ -1,3 +1,9 @@
 public class Main {
     
+    public static void main(String[] args) {
+        Toy toy1 = new Toy();
+                Toy toy2 = new Toy();
+
+       //  System.out.println(toy1.name);
+    }
 }
--- Toy.java
+++ Toy.java
@@ -1,12 +1,21 @@
 public class Toy{
 
-public String name;
-String brand ;
+public String name = "Ken";
+public String brand = "barbie";
 double price;
 int quantity;
 
+
+void setPrice(double price){
+    this.price = price;
+}
+
 public static void main(String[] args) {
     System.out.println("hello java trainees");
+    Toy toy1 = new Toy();
+        Toy toy2 = new Toy();
+
+        System.out.println(toy1.name);
 }
 
 
